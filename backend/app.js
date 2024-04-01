571166e607eed7cdbe4de73837552b90d71a94c7
Sunny Kumar <goodluck14115@gmail.com>
2024-04-01 23:27:37 +0530

    Connect backend to MongoDB

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,21 @@ const morgan = require("morgan");
 const cors = require("cors");
 const productRouter = require("./controllers/products");
 const middleware = require("./utils/middleware");
+const config = require("./utils/config");
+const { info, error } = require("./utils/logger");
+const mongoose = require("mongoose");
 
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(cors());
+
+const mongoUrl = config.MONGODB_URI;
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    info("Connected to MongoDB");
+  })
+  .catch((err) => error(`Error connect to MongoDb: ${err.message}`));
 
 app.use("/api/products", productRouter);
 
