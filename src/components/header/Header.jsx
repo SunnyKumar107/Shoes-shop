ccbc3a203db6c9bd0aadabab321f0f6db4402f3c
Sunny Kumar <goodluck14115@gmail.com>
2024-04-15 13:37:08 +0530

    Move search bar from header to home component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,12 @@
 import Styles from './Header.module.css'
-import { useDispatch, useSelector } from 'react-redux'
-import { searchByText } from '../../reducers/productReducer'
 import { NavLink } from 'react-router-dom'
 import propTypes from 'prop-types'
 import { useEffect, useState } from 'react'
 import User from './user/User'
+import { useSelector } from 'react-redux'
 
 function Header({ cartItems }) {
   const user = useSelector((state) => state.user)
-  const dispatch = useDispatch()
   const [showUserInfo, setShowUserInfo] = useState(false)
 
   useEffect(() => {
@@ -36,28 +34,19 @@ function Header({ cartItems }) {
           <img src="../../../shoesshop-logo.png" alt="shoes-shop-logo" />
         </div>
       </NavLink>
-      <div className={Styles.header_container}>
-        <div className={Styles.header_search}>
-          <input
-            type="text"
-            className={Styles.search_input}
-            placeholder="Enter your search shoes"
-            onChange={(e) => dispatch(searchByText(e.target.value))}
-          />
-        </div>
-        <div className={Styles.right_side}>
-          <NavLink to="/cart">
-            {cartItems.length !== 0 && <span>{cartItems.length}</span>}
-            <i className="fa-solid fa-cart-shopping"></i>
-          </NavLink>
-          <NavLink>
-            {showUserInfo ? (
-              <i className="fa-solid fa-xmark" onClick={toggleShow}></i>
-            ) : (
-              <i className="fa-solid fa-user" onClick={toggleShow}></i>
-            )}
-          </NavLink>
-        </div>
+
+      <div className={Styles.right_side}>
+        <NavLink to="/cart">
+          {cartItems.length !== 0 && <span>{cartItems.length}</span>}
+          <i className="fa-solid fa-cart-shopping"></i>
+        </NavLink>
+        <NavLink>
+          {showUserInfo ? (
+            <i className="fa-solid fa-xmark" onClick={toggleShow}></i>
+          ) : (
+            <i className="fa-solid fa-user" onClick={toggleShow}></i>
+          )}
+        </NavLink>
       </div>
       {showUserInfo && <User />}
     </div>
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,7 +4,7 @@ import SideBar from '../sideBar/SideBar'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
-import { initializeProducts } from '../../reducers/productReducer'
+import { initializeProducts, searchByText } from '../../reducers/productReducer'
 
 function Home() {
   const products = useSelector((state) => state.products)
@@ -23,6 +23,14 @@ function Home() {
   return (
     <div>
       <SideBar />
+      <div className={Styles.home_search}>
+        <input
+          type="text"
+          className={Styles.search_input}
+          placeholder="Enter your search shoes"
+          onChange={(e) => dispatch(searchByText(e.target.value))}
+        />
+      </div>
       <div className={Styles.card_container}>
         {products.map((product) => (
           <Card product={product} key={product.id} />
